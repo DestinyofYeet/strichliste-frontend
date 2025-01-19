da0e67746e2905fe7121e4bed550480095cf413d
DestinyofYeet <olebend@gmail.com>
2025-01-19 08:37:43 +0100

    plays sound randomly

--- src/services/sound.ts
+++ src/services/sound.ts
@@ -1,11 +1,40 @@
 import { Howl } from 'howler';
 import { CreateTransactionParams } from '../store/reducers';
 
-const dispense = new Howl({
-  src: ['ka-ching.wav'],
-});
+function getRand(list: Array<string>) {
+  return list[Math.floor(Math.random() * list.length)]
+}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function playCashSound(_params?: CreateTransactionParams): void {
   dispense.play();
+const baseFiles = [
+  "ka-ching.wav",
+  "mario-coin.wav"
+];
+
+function getDispense(file?: string) {
+  if (file == null) {
+    file = getRand(baseFiles);
+  }
+  return new Howl({ src: [file]});
+}
+
+const soundFiles = new Map<number, Array<string>>([
+  // [1, ["mario-coin.wav"]]
+]);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function playCashSound(_params?: CreateTransactionParams): void {
+  if (_params != null && _params.articleId != null) {
+    const articleId = _params.articleId;
+    const soundFileArray = soundFiles.get(articleId);
+    if (soundFileArray != null) {
+      getDispense(getRand(soundFileArray)).play();
+    } else {
+      getDispense(getRand(baseFiles)).play();
+    }
+  } else {
+    getDispense(getRand(baseFiles)).play();
+  }
 }
